b29ba6a4c92aed00b83ccd892a4b5bfd508dfe2c
StephenKingg <kingze1992@gmail.com>
2017-06-11 23:28:57 +0800

    update

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { PAGE } from '../../Routes';
+
+const propTypes = {
+  isTime: PropTypes.bool,
+};
+
+const defaultProps = {
+  isTime: true,
+};
 
 export default class Home extends Component {
   constructor(props) {
@@ -15,7 +24,7 @@ export default class Home extends Component {
     this.setState({
       elem: e,
     });
-    console.log(this.state.elem);
+    console.log(this.props.isTime, this.state.elem);
   }
 
   render() {
@@ -48,16 +57,19 @@ export default class Home extends Component {
     const elem = (
       <div>
         <div>Hello, Home</div>
-        <div className="menu">
-          <Link to={`${PAGE.home}`} onClick={this.handleClick}>{PAGE.home}</Link>
-          <Link to={`${PAGE.page1}`} onClick={this.handleClick}>{PAGE.page1}</Link>
-          <Link to={`${PAGE.page2}`} onClick={this.handleClick}>{PAGE.page2}</Link>
-          <Link to={`${PAGE.page3}`} onClick={this.handleClick}>{PAGE.page3}</Link>
-          <Link to={`${PAGE.page4}`} onClick={this.handleClick}>{PAGE.page4}</Link>
-        </div>
+        <ul className="menu">
+          <li><Link to={`${PAGE.home}`} onClick={this.handleClick}>{PAGE.home}</Link></li>
+          <li><Link to={`${PAGE.page1}`} onClick={this.handleClick}>{PAGE.page1}</Link></li>
+          <li><Link to={`${PAGE.page2}`} onClick={this.handleClick}>{PAGE.page2}</Link></li>
+          <li><Link to={`${PAGE.page3}`} onClick={this.handleClick}>{PAGE.page3}</Link></li>
+          <li><Link to={`${PAGE.page4}`} onClick={this.handleClick}>{PAGE.page4}</Link></li>
+        </ul>
       </div>
     );
 
     return elem;
   }
 }
+
+Home.propTypes = propTypes;
+Home.defaultProps = defaultProps;
